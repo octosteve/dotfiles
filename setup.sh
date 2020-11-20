a7249f2b779b0703dfa7be344772e98b4b1b63bf
Steven Nunez <octosteve@github.com>
2020-11-20 16:55:47 -0500

    Update and rename setup to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,38 @@
-mkdir -p ~/.config/nvim
-ln -s $(pwd)/neovimrc ~/.config/nvim/init.vim
+#!/bin/bash
+if [[ -z "${CODESPACES}" ]]; then
+  curl -sL https://deb.nodesource.com/setup_15.x | bash -
+  add-apt-repository ppa:cpick/hub
+
+  apt-get update
+
+  apt install -y neovim direnv zsh hub build-essential nodejs
+
+  chsh -s /usr/bin/zsh
+
+  git clone https://github.com/vim/vim.git /tmp/vim
+  cd /tmp/vim/src
+  make
+  make install
+fi
+
+mkdir -p ~/.config/nvimln -s $(pwd)/neovimrc ~/.config/nvim/init.vim
 mkdir -p ~/.vim
 ln -s $(pwd)/vimrc ~/.vim/vimrc
+
 # Incase we're using regular vim
-ln -s $(pwd)/vimrc ~/.vimrc
-# Install minpac
+ln -s $(pwd)/vimrc ~/.vimrc# Install minpac
 git clone https://github.com/k-takata/minpac.git \
     ~/.vim/pack/minpac/opt/minpac
+
 git clone https://github.com/k-takata/minpac.git \
     ~/.config/nvim/pack/minpac/opt/minpac
-
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
 ln -s $(pwd)/zshrc ~/.zshrc
 ln -s $(pwd)/zshrc.local ~/.zshrc.local
 
 # Install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+~/.fzf/install --all
 
 # Setup bin dir for local binaries
 mkdir -p ~/bin
