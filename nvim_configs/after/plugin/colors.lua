c3d2deca14de8e07baed7094c8e8bf23c8a32a59
Steven Nunez <octosteve@github.com>
2023-03-01 23:17:40 -0500

    Use neovim test strategy

--- nvim_configs/after/plugin/colors.lua
+++ nvim_configs/after/plugin/colors.lua
@@ -2,6 +2,10 @@ function MakeItLookNice(color)
 	color = color or "nightfox"
 
 	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
 end
 
 MakeItLookNice()
--- nvim_configs/after/plugin/vim-test.lua
+++ nvim_configs/after/plugin/vim-test.lua
@@ -3,3 +3,5 @@ vim.keymap.set("n", "<Leader>T", ":TestFile<CR>", { silent = true })
 vim.keymap.set("n", "<Leader>a", ":TestSuite<CR>", { silent = true })
 vim.keymap.set("n", "<Leader>l", ":TestLast<CR>", { silent = true })
 vim.keymap.set("n", "<Leader>g", ":TestVisit<CR>", { silent = true })
+
+vim.g["test#strategy"] = "neovim"
